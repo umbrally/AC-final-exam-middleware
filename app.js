1bdc0b46344a140d80e42972068b806ed75fe2dd
Jung <realumbrally@gmail.com>
2019-07-25 16:22:42 +0800

    Modify middleware code

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ const app = express()
 const port = 3000
 const middleware = require('./lib/middleware.js')
 
+
 // 列出全部 Todo
 app.get('/', middleware, (req, res) => {
-
   res.send('列出全部 Todo')
 })
+
 
 // 新增一筆 Todo 頁面
 app.get('/new', middleware, (req, res) => {
@@ -15,16 +16,16 @@ app.get('/new', middleware, (req, res) => {
 })
 
 // 顯示一筆 Todo 的詳細內容
-app.get('/:id', middleware, (req, res) => {
+app.get('/:id', (req, res) => {
   res.send('顯示一筆 Todo')
 })
 
 // 新增一筆  Todo
-app.post('/', middleware, (req, res) => {
+app.post('/', (req, res) => {
   res.send('新增一筆  Todo')
 })
 
-app.delete('/:id/delete', middleware, (req, res) => {
+app.delete('/:id/delete', (req, res) => {
   res.send('刪除 Todo')
 })
 
