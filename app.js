53796f629d31519df4d1da95f228cebdcea39c1e
Jung Liu <realumbrally@gmail.com>
2019-08-06 17:47:32 +0800

    Modify app.js

--- app.js
+++ app.js
@@ -3,15 +3,16 @@ const app = express()
 const port = 3000
 const middleware = require('./lib/middleware.js')
 
+app.use(middleware)
 
 // 列出全部 Todo
-app.get('/', middleware, (req, res) => {
+app.get('/', (req, res) => {
   res.send('列出全部 Todo')
 })
 
 
 // 新增一筆 Todo 頁面
-app.get('/new', middleware, (req, res) => {
+app.get('/new', (req, res) => {
   res.send('新增 Todo 頁面')
 })
 
